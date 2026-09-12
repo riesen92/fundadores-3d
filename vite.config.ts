86cfa0d4eb95cd15622acd688b01d6ae38365f4c
Riesen92 <bernardolealc@gmail.com>
2026-09-12 00:27:07 -0300

    Corrige despliegue en GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-export default defineConfig({ plugins: [vue()] })
+export default defineConfig(({ mode }) => ({
+  base: mode === 'development' ? '/' : '/fundadores-3d/',
+  plugins: [vue()],
+}))
